6623eb72b838997d8b1b4a831f85da0223cb4dd2
Yufeng Lv <lvyufeng2007@hotmail.com>
2018-06-07 11:15:51 +0800

    taobao item

--- eb_crawler/spiders/taobao.py
+++ eb_crawler/spiders/taobao.py
@@ -5,7 +5,7 @@ import scrapy
 class TaobaoSpider(scrapy.Spider):
     name = 'taobao'
     allowed_domains = ['www.taobao.com']
-    start_urls = ['http://www.taobao.com/']
+    start_urls = ['https://item.taobao.com/item.htm?id=525730909486']
 
     def parse(self, response):
         pass
