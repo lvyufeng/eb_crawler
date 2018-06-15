6d856644870ae441f8576b0b0ce57e12253c2add
Lv Yufeng <lvyufeng@LvdeMac-mini.local>
2018-06-15 11:47:58 +0800

    manual import data util

--- eb_crawler/utils/import_urls.py
+++ eb_crawler/utils/import_urls.py
@@ -1,6 +1,6 @@
 
 import os
-from eb_crawler.utils.redis_op import insert_data
+from eb_crawler.utils.redis_op import insert_data,redis_connect
 from eb_crawler.utils.config_parse import config_parse
 
 def get_urls(file_path):
@@ -21,17 +21,24 @@ def find_url_type(urls):
     port = config.getint('redis','port')
     db = config.getint('redis','db')
 
-    for item in config.items('domin'):
-        domins[item[-1]] = item[0]
-    print(domins)
-    for url in urls:
-        for domin in domins.keys():
-            if url.find(domin):
-                try:
-                    type = config.get('url_type',domins[domin])
-                    insert_data(host,port,db,type,url)
-                except Exception:
-                    print(Exception)
-                    break
+    r = redis_connect(host,port,db)
+    if r != None:
+        for item in config.items('domin'):
+            domins[item[-1]] = item[0]
+        print(domins)
+        for url in urls:
+            for domin in domins.keys():
+                if url.find(domin):
+
+                    try:
+                        type = config.get('url_type',domins[domin])
+                        insert_data(r,type,url)
+                    except Exception as e:
+                        print(url)
+                        print(e)
+                        break
+        print('import finished')
+    else:
+        print('can not connect redis')
 urls = get_urls('/Users/lvyufeng/PycharmProjects/eb_crawler/eb_crawler/utils/taskinfo.csv')
 find_url_type(urls)
--- eb_crawler/utils/redis_op.py
+++ eb_crawler/utils/redis_op.py
@@ -1,10 +1,17 @@
 import configparser
 import redis
 
-def insert_data(host,port,db,type,str):
+def redis_connect(host,port,db):
     try:
         r = redis.Redis(host=host,port=port,db=db)
+        return r
     except:
         print('redis connect error')
-    else:
-        r.lpush(type,str)
+        return None
+def insert_data(r,type,str):
+    try:
+        r.lpush(type, str)
+    except:
+        print('redis insert error')
+
+
