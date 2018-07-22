fffe6f93f27da4ce42981630ab8159e5db4ed482
lvyufeng <lvyufeng2007@hotmail.com>
2018-07-22 08:37:07 +0800

    存储为mysql数据库

--- test/mongo_test.py
+++ test/mongo_test.py
@@ -6,21 +6,36 @@ collection = db['product_info']
 
 # collection.remove({'platform':'YouLeGou'})
 # # print(collection.distinct('platform'))
-print(collection.find({'platform':'SuNing'}).count())
+print(collection.find({'platform':'JingDong'}).count())
 # # lines = None
 # with open('taskinfo.csv','r') as f:
 #     lines = f.readlines()
 #     print(len(lines))
 #     for i in lines:
-#         if 'ule' in i:
-#             # print(i)
-#             # pass
-#             # if len(i.split(',')[-1].split('/')) == 5:
-#             id = i.split(',')[-1].split('/')[-1].split('-')[0]
-#             # print(id)
-#             collection.insert_one({
-#                 'sku_id':id,
-#                 'platform':'YouLeGou'
-#             })
+#         if 'jd' in i:
+#             id = i.split(',')[-1].split('/')[-1].split('.')[0]
+#             inner_id = i.split(',')[0].strip('"')
+#             # print(i,id,inner_id)
+#             # if collection.find_one({'productActualID': id}):
+#             item = {
+#                 'sku_id': id,
+#                 'inner_id':inner_id,
+#                 'platform':'JingDong'
+#             }
+#             collection.insert_one(item)
+#         elif  'suning' in i and len(i.split(',')[-1].split('/')) > 4:
+#             id = i.split(',')[-1].split('/')[-1].split('.')[0]
+#             shop_id = i.split(',')[-1].split('/')[-2]
+#             inner_id = i.split(',')[0].strip('"')
+#             # print(i, id, inner_id,shop_id)
+#             # if collection.find_one({'productActualID': id}):
+#             item = {
+#                 'sku_id': id,
+#                 'inner_id': inner_id,
+#                 'shop_id':shop_id,
+#                 'platform':'SuNing'
+#             }
+#             collection.insert_one(item)
+
 # #
 # f.close()
--- test/test_1.py
+++ test/test_1.py
@@ -3,4 +3,9 @@ import pymongo
 client = pymongo.MongoClient('localhost',27017)
 # client = pymongo.MongoClient('localhost',27017)
 test = client['test']
-pd = test['product_info']
+pd = test['test_tb']
+print(pd.find().count())
+#
+# test = [1,2,3]
+# t = (v for v in test)
+# print(t,(1,2,3))
